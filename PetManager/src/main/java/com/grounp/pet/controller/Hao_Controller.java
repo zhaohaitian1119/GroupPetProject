43401acf08b2a01ab58161bdc04813961d6c3fb8
zhaohaitian <397408662@qq.com>
2020-09-21 13:22:56 +0800

    修改了添加方法

--- PetManager/src/main/java/com/grounp/pet/controller/Hao_Controller.java
+++ PetManager/src/main/java/com/grounp/pet/controller/Hao_Controller.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author 蒿雨
@@ -17,7 +20,20 @@ public class Hao_Controller {
     Pet_Service pet_service;
 
     @RequestMapping("/add")
-    public String add(Pet pet) {
+    public String add(String petName, String petBreed,String petSex, String birthday, String description) {
+        Pet pet = new Pet();
+        pet.setPetName(petName);
+        pet.setPetBreed(petBreed);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        try {
+            date = simpleDateFormat.parse(birthday);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        pet.setBirthday(date);
+        pet.setPetSex(petSex);
+        pet.setDescription(description);
         int a = pet_service.add(pet);
         if (a != 0) {
             System.out.println("添加成功！");
